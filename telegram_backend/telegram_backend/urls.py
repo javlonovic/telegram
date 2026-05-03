4c5148e26a44e3705c26b2b9c9bd159d84c444cf
javlonovic <javlonovic@gmail.com>
2026-05-03 18:06:57 +0500

    fix healthcheck endpoint

--- telegram_backend/telegram_backend/urls.py
+++ telegram_backend/telegram_backend/urls.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+
+def health(request):
+    return JsonResponse({'status': 'ok'})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/health/', health),
 
     # API v1
     path('api/auth/', include('apps.users.urls.auth_urls')),
