20dce915f5f7ceb5ec38f5ab3079fdb7fb863fdb
javlonovic <javlonovic@gmail.com>
2026-05-03 18:03:00 +0500

    fix asgi app registry initialization order

--- telegram_backend/telegram_backend/asgi.py
+++ telegram_backend/telegram_backend/asgi.py
@@ -1,14 +1,19 @@
 import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'telegram_backend.settings')
+
 from django.core.asgi import get_asgi_application
+
+# Initialize Django app registry before importing anything that uses models
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import apps.messaging.routing as messaging_routing
 import apps.users.routing as presence_routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'telegram_backend.settings')
-
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket': AuthMiddlewareStack(
         URLRouter(
             messaging_routing.websocket_urlpatterns +
